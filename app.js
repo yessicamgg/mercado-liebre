5bcfe65490e56c874bedf4826eaa411193ced54a
yessica <yessicamgonzalezg@gmail.com>
2023-07-17 19:13:20 -0400

    [ADD] primer commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+const dotenv = require("dotenv").config();
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/home.html'))
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'))
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'))
+})
+
+app.listen(process.env.PORT, () => {
+    console.log("servidor escuchando en puerto " + process.env.PORT + " http://localhost:3000" )
+})
