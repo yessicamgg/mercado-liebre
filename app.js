7a397958ade6d23abb1849f389c0ecad292d1841
yessica <yessicamgonzalezg@gmail.com>
2023-07-18 21:15:38 -0400

    cambios en json

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.get('/home', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'))
 })
 
